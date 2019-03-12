bd65430c340e972f9f718dc63e8795c565d68a1b
imageprom <necris@imageprom.com>
2019-03-12 17:21:01 +0300

    Ошибка выбора файла

--- nx.highload.list_1.0/templates/.default/result_modifier.php
+++ nx.highload.list_1.0/templates/.default/result_modifier.php
@@ -3,15 +3,17 @@
 // Если поле UF_FILE содержит файл
 
 foreach ($arResult['ITEMS'] as $code => &$arItem) {
-	$arItem = CFile::GetFileArray($arItem['~UF_FILE']);
-	$file['FORMAT'] = pathinfo($file['FILE_NAME'], PATHINFO_EXTENSION);
-	$file['SIZE'] = round($file['FILE_SIZE'] / 1024, 2);
-	$file['UNIT'] = 'кб';
-	 if($file['SIZE'] >= 1000 ) {
-	    $file['SIZE'] = round($file['FILE_SIZE']/(1024*1024),2);
-	    $file['UNIT'] = 'мб';
-	 }
+	if($arItem['~UF_FILE']) {
+		$file = CFile::GetFileArray($arItem['~UF_FILE']);
+		$file['FORMAT'] = pathinfo($file['FILE_NAME'], PATHINFO_EXTENSION);
+		$file['SIZE'] = round($file['FILE_SIZE'] / 1024, 2);
+		$file['UNIT'] = 'кб';
+		 if($file['SIZE'] >= 1000 ) {
+			$file['SIZE'] = round($file['FILE_SIZE']/(1024*1024),2);
+			$file['UNIT'] = 'мб';
+		 }
 
-	$arItem['FILE'] = $file;
+		$arItem['FILE'] = $file;
+	}
 }
 ?>
